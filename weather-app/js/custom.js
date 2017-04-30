c4e61e658ede36c74d227306c4e7586299964d6e
stepforwardtech <jrstrayhorn@gmail.com>
2017-04-29 23:47:25 -0400

    refactoring js code into module pattern

--- weather-app/js/custom.js
+++ weather-app/js/custom.js
@@ -1,62 +1,74 @@
+
+var Services = function () {
+    var getCurrentLocation = function (done) {
+        $.get("http://ipinfo.io?token=23633d0f168caf", done, "jsonp");
+    };
+
+    var getWeather = function (lat, long, done) {
+        $.get("http://api.openweathermap.org/data/2.5/weather?" + 
+                "lat=" + lat + 
+                "&lon=" + long + 
+                "&APPID=224b7da19b9e9c30983d4c504b9c491e", 
+                done, 
+                "jsonp");
+    };
+
+    return {
+        getCurrentLocation: getCurrentLocation,
+        getWeather: getWeather
+    }
+}();
+
+var WeatherApp = function(services) {
+
+    var init = function () {
+        $("#Fdeg-button").click(showFahrenheit);
+        $("#Cdeg-button").click(showCelsius);
+        loadWeather();
+    };
+
+    var showFahrenheit = function () {
+        $("#temp-display").text(convertToFahrenheit($("#temp-display").attr("data-temp")) + " \xB0 F");
+    };
+
+    var showCelsius = function() {
+        $("#temp-display").text(convertToCelsius($("#temp-display").attr("data-temp")) + " \xB0 C");
+    };
+
+    var loadWeather = function () {
+         services.getCurrentLocation(locationDone);
+    };
+
+    var locationDone = function(response) {
+        var latLongArray = response.loc.split(',');
+        services.getWeather(latLongArray[0], latLongArray[1], weatherDone);
+    };
+
+    var weatherDone = function(response) {      
+        $("#weather-image").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+        $(".weather-desc").text(capitalizeFirstLetter(response.weather[0].description));   
+        $("#temp-display").attr("data-temp", response.main.temp);
+        showFahrenheit();
+    };
+
+    var convertToFahrenheit = function (temp) {
+        return ((1.8 * (temp - 273))  + 32) | 0;
+    };
+
+    var convertToCelsius = function (temp) {
+        return (temp - 273.15) | 0;
+    };
+
+    var capitalizeFirstLetter = function (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
+    return {
+        init: init
+    }
+}(Services);
+
 $(document).ready(function() {
-    $("#Fdeg-button").click(function () {
-        var temp = $("#temp").attr("data-temp");
-        var fTemp = ConvertToFahrenheit(temp);
-        $("#temp").text(fTemp + " \xB0 F");
-    });
-
-    $("#Cdeg-button").click(function() {
-        var temp = $("#temp").attr("data-temp");
-        var cTemp = ConvertToCelsius(temp);
-        $("#temp").text(cTemp + " \xB0 C");
-    });
-
-/*
-    var response = {
-        "coord":{"lon":139,"lat":35},
-        "sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
-        "weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
-        "main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
-        "wind":{"speed":7.31,"deg":187.002},
-        "rain":{"3h":0},
-        "clouds":{"all":92},
-        "dt":1369824698,
-        "id":1851632,
-        "name":"Shuzenji",
-        "cod":200
-    };*/
-    
-    $.get("http://ipinfo.io?token=23633d0f168caf", function(response) {
-        var latLongString = response.loc;
-        var latLongArray = latLongString.split(',');
-        var lat = latLongArray[0];
-        var long = latLongArray[1];
-
-        $.get("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=224b7da19b9e9c30983d4c504b9c491e", function(response) {
-            
-            var img = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"; 
-            $("#weather-image").attr("src", img);
-
-            var desc = response.weather[0].description;
-            $(".weather-desc").text(capitalizeFirstLetter(desc));
-            
-            var temp = response.main.temp;
-            $("#temp").attr("data-temp", temp);
-            var fTemp = ConvertToFahrenheit(temp);
-            $("#temp").text(fTemp + " \xB0 F");
-
-        }, "jsonp");
-    }, "jsonp");
+    WeatherApp.init();  
 });
 
-function ConvertToFahrenheit(temp) {
-    return ((1.8 * (temp - 273))  + 32) | 0;
-}
-
-function ConvertToCelsius(temp) {
-    return (temp - 273.15) | 0;
-}
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
