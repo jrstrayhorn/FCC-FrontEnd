457da8d13183ec598f9522aed135afcfe4494373
stepforwardtech <jrstrayhorn@gmail.com>
2017-05-05 18:25:11 -0400

    adding footer to html, refactoring js code, changing look of offline results

--- twitch-json/js/custom.js
+++ twitch-json/js/custom.js
@@ -1,50 +1,62 @@
 var html = "";
 
-var userArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404", "brunofin"]; 
+var userArray = ["ESL_SC2", "OgamingSC2", "freecodecamp", "noobs2ninjas", "comster404", "brunofin", "eleaguetv"]; 
 
 $(document).ready(function() {
-    
    checkUser(0);
-
 });
 
 function checkUser(i) {
     if (i < userArray.length) {
         var user = userArray[i];
-        $.get("https://wind-bow.gomix.me/twitch-api/streams/" + 
-                user , function(response) {
-
-                    var myResponse = response;
-
-    if (myResponse.stream) {
-        html += "<div class='alert alert-success' role='alert'>";
-            html += "<strong>" + myResponse.stream.channel.display_name + "</strong> is <a href='" + myResponse.stream.channel.url + "' class='alert-link' target='_blank'>ONLINE (click here to view)</a>";
-            html += " streaming '" + myResponse.stream.channel.status + "'";
-        html += "</div>";
-        $(".twitch-results").html(html);
-        checkUser(i+1);
-    }
-    else {
-        // check if user exists
-        $.get("https://wind-bow.gomix.me/twitch-api/users/" + user, function(userResponse) {
-            var myUserResponse = userResponse;
-        if (!myUserResponse.error) {
-            html += "<div class='alert alert-warning' role='alert'>";
-            html += "<strong>" + myUserResponse.display_name + "</strong> is OFFLINE";
-                html += "</div>";
-        } else {
-            html += "<div class='alert alert-danger' role='alert'>";
-            html += "<strong>" + user + "</strong> does not exist.";
-                html += "</div>";
-        }
-        $(".twitch-results").html(html);
-        checkUser(i+1);
+        $.get("https://wind-bow.gomix.me/twitch-api/streams/" + user , function(response) {
+            var myResponse = response;
+            if (myResponse.stream) {
+                html += GetOnlineHTML(myResponse.stream.channel.display_name, myResponse.stream.channel.url, myResponse.stream.channel.status);
+                $(".twitch-results").html(html);
+                checkUser(i+1);
+            }
+            else {
+                // check if user exists
+                $.get("https://wind-bow.gomix.me/twitch-api/users/" + user, function(userResponse) {
+                    var myUserResponse = userResponse;
+                    if (!myUserResponse.error) {
+                        html += GetOfflineHTML(myUserResponse.display_name, myUserResponse.name);
+                    }
+                    else {
+                        html += GetNotFoundHTML(user);
+                    }
+                    $(".twitch-results").html(html);
+                    checkUser(i+1);
+                }, "jsonp");
+            }
         }, "jsonp");
-        
     }
+}
 
-            }, "jsonp");
-    }
+function GetOnlineHTML(displayName, url, channelStatus) {
+    var returnHTML = "";
+    returnHTML += "<div class='alert alert-success' role='alert'>";
+    returnHTML += "<strong>" + displayName + "</strong> is <a href='" + url + "' class='alert-link' target='_blank'>ONLINE (click here to view)</a>";
+    returnHTML += " streaming '" + channelStatus + "'";
+    returnHTML += "</div>";
+    return returnHTML;
+}
+
+function GetOfflineHTML(displayName, name) {
+    var returnHTML = "";
+    returnHTML += "<div class='alert alert-warning' role='alert'>";
+    returnHTML += "<strong>" + displayName + "</strong> is <a href='https://www.twitch.tv/" + name + "' class='alert-link' target='_blank'>OFFLINE (click here to view channel)</a>";
+    returnHTML += "</div>";
+    return returnHTML;
+}
+
+function  GetNotFoundHTML(userName) {
+    var returnHTML = "";
+    returnHTML += "<div class='alert alert-danger' role='alert'>";
+    returnHTML += "<strong>" + userName + "</strong> does not exist.";
+    returnHTML += "</div>";
+    return returnHTML;
 }
 
   
