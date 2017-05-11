92c3b653a494dced69055ef9564e4990780f00fa
stepforwardtech <jrstrayhorn@gmail.com>
2017-05-11 19:49:55 -0400

    fixing bugs - decimal point entry, math symbols as first entry, clear calculator before starting another calculation

--- js-calculator/js/custom.js
+++ js-calculator/js/custom.js
@@ -26,9 +26,16 @@ function processMath(math) {
 function doMath() {
     var sum = 0;
 
-    // strip math symbols from end of calc-text
+    
+    var operation = $('.calc-text').text();
 
-    var pipeCalc = $('.calc-text').text().replace(/[^.0-9]/g, replacer); 
+    // strip math symbols from end of calc-text
+    var lastCharCode = operation.substring(operation.length-1).charCodeAt(0);
+    if (mathCodes.indexOf(lastCharCode) !== -1) {
+        operation = operation.substring(0, operation.length-1);
+    }
+
+    var pipeCalc = operation.replace(/[^.0-9]/g, replacer); 
     var calcArr = pipeCalc.split('|');
 
     for (var i = 0; i < calcArr.length; i++) {
@@ -60,9 +67,54 @@ function doMath() {
         sum += performMath(finalArr[x], finalArr[x+2], finalArr[x+1].charCodeAt(0));
     }*/
 
-    sum = performMath(parseFloat(finalArr[0]), parseFloat(finalArr[2]), finalArr[1].charCodeAt(0));
+    // Processing multiplication and division left to right
+    while (finalArr.indexOf(String.fromCharCode(MUL_CODE)) !== -1 || finalArr.indexOf(String.fromCharCode(DIV_CODE)) !== -1) {
+        for (var x = 0; x < finalArr.length; x++) {
+            var sym_code = finalArr[x].charCodeAt(0);
+            if (sym_code === MUL_CODE || sym_code === DIV_CODE) {
+                var myX = parseFloat(finalArr[x-1]);
+                var y = parseFloat(finalArr[x+1]);
+                var mySum = performMath(myX, y, sym_code);
+                finalArr[x-1] = mySum.toString();
+                var newArr = finalArr.filter(function(el, index) {
+                    var keep = true;
 
-    $('.answer-text').text(sum);
+                    if (index === x || index === x+1)
+                        keep = false;
+
+                    return keep;
+                });
+                finalArr = newArr;
+                break;
+            }
+        }
+    }
+
+    // Processing addition and subtraction left to right
+    while (finalArr.indexOf(String.fromCharCode(SUB_CODE)) !== -1 || finalArr.indexOf(String.fromCharCode(ADD_CODE)) !== -1) {
+        for (var x = 0; x < finalArr.length; x++) {
+            var sym_code = finalArr[x].charCodeAt(0);
+            if ((sym_code === SUB_CODE || sym_code === ADD_CODE) && finalArr[x].length === 1) {
+                var myX = parseFloat(finalArr[x-1]);
+                var y = parseFloat(finalArr[x+1]);
+                var mySum = performMath(myX, y, sym_code);
+                finalArr[x-1] = mySum.toString();
+                var newArr = finalArr.filter(function(el, index) {
+                    var keep = true;
+
+                    if (index === x || index === x+1)
+                        keep = false;
+
+                    return keep;
+                });
+                finalArr = newArr;
+                break;
+            }
+        }
+    }
+
+    $('.answer-text').text(finalArr[0]);
+    $('.calc-text').text(operation);
 }
 
 function performMath(x, y, sym_code) {
@@ -92,11 +144,33 @@ function replacer(match) {
 }
 
 function addToOperations(math) {
+    
+
+    if ($('.calc-text').text().indexOf('input') == -1 && $('.answer-text').text() !== "0") {
+        clearCalculator();
+    }
+    
     var operation = $('.calc-text').text();
-    if (operation.indexOf('input') !== -1)
-        operation = '';
-    var lastCharCode = operation.substring(operation.length-1).charCodeAt(0);
     var mathCode = math.charCodeAt(0);
+
+    if (operation.indexOf('input') !== -1)
+    {
+        if (mathCodes.indexOf(mathCode) !== -1)
+        {
+            if (mathCode !== SUB_CODE)
+            {
+                return;
+            }
+        }
+        operation = '';
+    } 
+    else 
+    {
+        
+    }
+        
+    var lastCharCode = operation.substring(operation.length-1).charCodeAt(0);
+    
     if (mathCodes.indexOf(lastCharCode) !== -1)
     {  
         // check - can't have same symbol back to back
@@ -119,6 +193,16 @@ function addToOperations(math) {
             }
         }
     }
+
+    if (mathCode === DEC_CODE && !canAddDecimal)
+        return;
+
+    if (mathCode === DEC_CODE)
+        canAddDecimal = false;
+
+    if (mathCodes.indexOf(mathCode) !== -1)
+        canAddDecimal = true;
+    
     operation += math;
     $('.calc-text').text(operation);
     //alert(math.charCodeAt(0));
@@ -128,7 +212,9 @@ var ADD_CODE = 43;
 var SUB_CODE = 45;
 var MUL_CODE = 215;
 var DIV_CODE = 247;
+var DEC_CODE = 46;
 var mathCodes = [DIV_CODE, MUL_CODE, SUB_CODE, ADD_CODE];
+var canAddDecimal = true;
 
 function clearCalculator() {
     $('.calc-text').text('Waiting for input...');
